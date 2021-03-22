1d0ec038ef8a7b0501452f02e95f1bf0c61a5516
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-22 06:47:58 +0200

    new changes

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -8,9 +8,10 @@ export class TokenInterceptorService implements HttpInterceptor {
   constructor(private injector: Injector){}
   intercept(req, next) {
     let authService = this.injector.get(AuthService)
+    let token =  authService.getToken() || "";
     let tokenizedReq = req.clone(
       {
-        headers: req.headers.set('Authorization', 'bearer ' + authService.getToken())
+        headers: req.headers.set('Authorization', 'bearer ' +token)
       }
     )
     return next.handle(tokenizedReq)
